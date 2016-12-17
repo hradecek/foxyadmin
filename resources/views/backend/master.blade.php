717b437435065150a42b4bdc0440e7eba1d738ed
Ivo Hradek <ivohradek@gmail.com>
2016-12-17 20:55:39 +0100

    Added sidebar

--- resources/views/backend/master.blade.php
+++ resources/views/backend/master.blade.php
@@ -12,10 +12,19 @@
 <div class="page-container">
   @include('backend.sidebar')
 
-  <div class="page-content">
-    <h1 class="page-content-title">
-      Sample Text <small>sample text</small>
-    </h1>
+  <div class="page-content-wrapper">
+    <div class="page-content">
+      <h1 class="page-content-title">
+        Sample Text <small>sample text</small>
+      </h1>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">Sample</div>
+          <div class="row">Row</div>
+          <div class="row">Content</div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </body>
---
+++ resources/views/backend/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="page-sidebar navbar-collapse collapse in">
+  <ul class="sidebar-menu">
+    <li class="sidebar-item">
+      <a href="javascript:;" class="sidebar-link navbar-toggle">
+        <i class="icon-diamond"></i>
+        <span class="sidebar-item-title">__ITEM__</span>
+        <span class="arrow open"></span>
+      </a>
+      <ul class="sidebar-sub-menu" style="display: block;">
+        <li class="sidebar-item">
+          <a href="__HREF__" class="nav-link">
+            <span class="sidebar-item-title">__ITEM__</span>
+          </a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</div>
