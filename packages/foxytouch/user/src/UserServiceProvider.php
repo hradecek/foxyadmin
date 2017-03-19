43d72861167e8918dde04eb4c69d7e19f572bb0c
Ivo Hradek <ivohradek@gmail.com>
2017-03-19 23:38:04 +0100

    Fix Collective\Html provider register

--- packages/foxytouch/user/src/UserServiceProvider.php
+++ packages/foxytouch/user/src/UserServiceProvider.php
@@ -39,9 +39,9 @@ class UserServiceProvider extends ServiceProvider
 
         $this->package(__DIR__, 'users');
         $aliases = [
-            'Form' => 'Collective\Html\FormFacade',
-            'Html' => 'Collective\Html\HtmlFacade',
-            'Image' => 'Intervention\Image\Facades\Image'
+            'Form' => \Collective\Html\FormFacade::class,
+            'Html' => \Collective\Html\HtmlFacade::class,
+            'Image' => \Intervention\Image\Facades\Image::class
         ];
         $this->loadAliases($aliases);
     }
@@ -54,7 +54,8 @@ class UserServiceProvider extends ServiceProvider
     public function register()
     {
         Log::info('[UserServiceProvider] Registering');
-        $this->app->register('Intervention\Image\ImageServiceProvider');
+        $this->app->register(\Intervention\Image\ImageServiceProvider::class);
+        $this->app->register(\Collective\Html\HtmlServiceProvider::class);
         Log::info('[UserServiceProvider] Binding');
         $this->app->bind(UserRepository::class, function() {
             return new EloquentUserRepository(new User);
